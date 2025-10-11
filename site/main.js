412f0159e7b2575c89c1500defd5ea3bbf0a90ef
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-11 15:06:19 -0400

    Basic scene with light working

--- site/main.js
+++ site/main.js
@@ -2,21 +2,49 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+// Setting up renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Creating scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(7,4,2)
+camera.rotation.set(0,Math.PI/2,0)
 
-camera.position.z = 5;
 
+// Setting up camera controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Load the scene model
+const loader = new GLTFLoader();
+loader.load( '/scene.glb', function ( gltf ) {
+  scene.add( gltf.scene );
+  controls.target.set(gltf.scene.position.x, gltf.scene.position.y, gltf.scene.position.z);
+}, undefined, function ( error ) {
+  console.error( error );
+} );
+
+// Adding a light to the scene
+const light = new THREE.PointLight(0xffff00, 2)
+light.position.x = 1.0
+light.position.y = 3.0
+light.position.z = -1.0
+light.castShadow = false
+light.decay = 0.5
+scene.add(light)
+
+//const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//const cube = new THREE.Mesh( geometry, material );
+//cube.position.z = -5
+//scene.add( cube );
+
+// Animate function
 function animate() {
   renderer.render( scene, camera );
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate );
