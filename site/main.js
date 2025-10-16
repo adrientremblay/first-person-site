0a8f2b458f08d169189871e8fb7be94bd8a403b6
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-16 13:54:05 -0400

    fixed function to resize window

--- site/main.js
+++ site/main.js
@@ -183,7 +183,7 @@ const updateScreenVisibility = () => {
 const onResize = () => {
   cssRenderer.setSize(window.innerWidth, window.innerHeight);
   webGlRenderer.setSize( window.innerWidth, window.innerHeight );
-  //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.aspect = window.innerWidth / window.innerHeight;
 };
 
 // Animate function
