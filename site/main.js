30093b60bc98153166c815251293d075f5f74e8a
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-16 13:34:34 -0400

    Moved the rain shader plane behind the window

--- site/main.js
+++ site/main.js
@@ -142,13 +142,14 @@ const init = () => {
   rainAudio.play(); 
 
   // Creating the window mesh
-  const windowGeometry = new THREE.PlaneGeometry(5,5);
+  const windowGeometry = new THREE.PlaneGeometry(4,4);
   rainyWindowMaterial = new THREE.ShaderMaterial({
     uniforms: {
       u_time: { value: 0 },
       u_noiseScale: { value: 20.0 },
       u_streakIntensity: { value: 0.5 },
       u_scrollSpeed: { value: 1.0},
+      u_seed: { value: Math.random()},
     },
     transparent: true,
     vertexShader: `
@@ -163,28 +164,31 @@ const init = () => {
       uniform float u_noiseScale;
       uniform float u_streakIntensity;
       uniform float u_scrollSpeed;
+      uniform float u_seed;
       varying vec2 vUv;
 
-      // Simple 2D noise (could replace with classic perlin/simplex)
       float hash(vec2 p) {
-        return fract(sin(dot(p, vec2(127.1,311.7))) * 43758.5453);
+        return fract(sin(dot(p, vec2(127.1,311.7)) + u_seed) * 43758.5453);
       }
 
       float noise(vec2 p){
+        // Extract the integer and float components of the vec p
         vec2 i = floor(p);
         vec2 f = fract(p);
+        // Get the four points of the lattice
         float a = hash(i);
         float b = hash(i + vec2(1.0, 0.0));
         float c = hash(i + vec2(0.0, 1.0));
         float d = hash(i + vec2(1.0, 1.0));
-        vec2 u = f*f*(3.0-2.0*f);
-        return mix(a, b, u.x) + (c - a)*u.y*(1.0 - u.x) + (d - b)*u.x*u.y;
+        vec2 u = f*f*(3.0-2.0*f); // smoothstep interpolation
+        //vec2 u = f; // linear interpolation
+        return mix(a, b, u.x) + (c - a)*u.y*(1.0 - u.x) + (d - b)*u.x*u.y; // billinear interpolation expanded and simplified formula
       }
 
       void main() {
         vec2 uv = vUv;
-        float n = noise(vec2(uv.x * u_noiseScale, uv.y * u_noiseScale + u_time * u_scrollSpeed));
-        float streaks = smoothstep(0.45, 0.5, n);
+        float n = noise(vec2(uv.x * 80.0, uv.y * 20.0 + u_time * u_scrollSpeed));
+        float streaks = smoothstep(0.6, 0.9, n);
         float alpha = streaks * u_streakIntensity;
 
         gl_FragColor = vec4(vec3(0.8, 0.9, 1.0) * alpha, alpha);
@@ -192,7 +196,7 @@ const init = () => {
     `
   });
   const plane = new THREE.Mesh(windowGeometry, rainyWindowMaterial);
-  plane.position.set(-1,4,0);
+  plane.position.set(-4.1,4,4.6);
   plane.rotateY(Math.PI/2);
   scene.add(plane);
 };
