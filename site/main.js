b63893a8839120f93128c32ed21b9ae63d078a1f
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-16 13:51:33 -0400

    extracted rain to it's own class

--- site/main.js
+++ site/main.js
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 import CameraControls from 'camera-controls';
 CameraControls.install( { THREE: THREE } );
+import {RainyWindow} from './rainy_window.js';
 
 // Constants
 const CLOCK = new THREE.Clock();
@@ -23,11 +24,8 @@ var div;
 // For looking at the screen
 var viewScreenPosition = new THREE.Vector3();
 var viewScreenQuat;
-// For returning back to orginal camera position
-var originalCameraPosition = new THREE.Vector3();
-var originalCameraQuat = new THREE.Quaternion();
 // rainy window
-var rainyWindowMaterial;
+var rainyWindow;
 
 // Renderers
 var cssRenderer;
@@ -136,69 +134,10 @@ const init = () => {
   // Hooking up on resize
   window.addEventListener('resize', onResize);
 
-  // Play ambient rain audio
-  let rainAudio = new Audio('rain_on_window.mp3'); 
-  rainAudio.loop = true;
-  rainAudio.play(); 
+  // setting up rainy window
+  rainyWindow = new RainyWindow()
+  scene.add(rainyWindow);
 
-  // Creating the window mesh
-  const windowGeometry = new THREE.PlaneGeometry(4,4);
-  rainyWindowMaterial = new THREE.ShaderMaterial({
-    uniforms: {
-      u_time: { value: 0 },
-      u_noiseScale: { value: 20.0 },
-      u_streakIntensity: { value: 0.5 },
-      u_scrollSpeed: { value: 1.0},
-      u_seed: { value: Math.random()},
-    },
-    transparent: true,
-    vertexShader: `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `,
-    fragmentShader: `
-      uniform float u_time;
-      uniform float u_noiseScale;
-      uniform float u_streakIntensity;
-      uniform float u_scrollSpeed;
-      uniform float u_seed;
-      varying vec2 vUv;
-
-      float hash(vec2 p) {
-        return fract(sin(dot(p, vec2(127.1,311.7)) + u_seed) * 43758.5453);
-      }
-
-      float noise(vec2 p){
-        // Extract the integer and float components of the vec p
-        vec2 i = floor(p);
-        vec2 f = fract(p);
-        // Get the four points of the lattice
-        float a = hash(i);
-        float b = hash(i + vec2(1.0, 0.0));
-        float c = hash(i + vec2(0.0, 1.0));
-        float d = hash(i + vec2(1.0, 1.0));
-        vec2 u = f*f*(3.0-2.0*f); // smoothstep interpolation
-        //vec2 u = f; // linear interpolation
-        return mix(a, b, u.x) + (c - a)*u.y*(1.0 - u.x) + (d - b)*u.x*u.y; // billinear interpolation expanded and simplified formula
-      }
-
-      void main() {
-        vec2 uv = vUv;
-        float n = noise(vec2(uv.x * 80.0, uv.y * 20.0 + u_time * u_scrollSpeed));
-        float streaks = smoothstep(0.6, 0.9, n);
-        float alpha = streaks * u_streakIntensity;
-
-        gl_FragColor = vec4(vec3(0.8, 0.9, 1.0) * alpha, alpha);
-      }
-    `
-  });
-  const plane = new THREE.Mesh(windowGeometry, rainyWindowMaterial);
-  plane.position.set(-4.1,4,4.6);
-  plane.rotateY(Math.PI/2);
-  scene.add(plane);
 };
 
 // On mouse move event for zooming into the screen
@@ -260,7 +199,7 @@ const animate = () => {
     webGlRenderer.render( scene, camera );
     cssRenderer.render(scene, camera);   // iframe //scene.rotation.x += 0.01
 
-  rainyWindowMaterial.uniforms.u_time.value = performance.now() / 1000;
+  rainyWindow.material.uniforms.u_time.value = performance.now() / 1000;
 };
 //controls.addEventListener('change', animate); // only re-render when camera moves
 //animate() // Animate the first frame
---
+++ site/rainy_window.js
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+
+export class RainyWindow extends THREE.Mesh {
+   static vertexShader = `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `;
+
+    static fragmentShader = `
+        uniform float u_time;
+        uniform float u_noiseScale;
+        uniform float u_streakIntensity;
+        uniform float u_scrollSpeed;
+        uniform float u_seed;
+        varying vec2 vUv;
+
+        float hash(vec2 p) {
+            return fract(sin(dot(p, vec2(127.1,311.7)) + u_seed) * 43758.5453);
+        }
+
+        float noise(vec2 p){
+            // Extract the integer and float components of the vec p
+            vec2 i = floor(p);
+            vec2 f = fract(p);
+            // Get the four points of the lattice
+            float a = hash(i);
+            float b = hash(i + vec2(1.0, 0.0));
+            float c = hash(i + vec2(0.0, 1.0));
+            float d = hash(i + vec2(1.0, 1.0));
+            vec2 u = f*f*(3.0-2.0*f); // smoothstep interpolation
+            //vec2 u = f; // linear interpolation
+            return mix(a, b, u.x) + (c - a)*u.y*(1.0 - u.x) + (d - b)*u.x*u.y; // billinear interpolation expanded and simplified formula
+        }
+
+        void main() {
+            vec2 uv = vUv;
+            float n = noise(vec2(uv.x * 80.0, uv.y * 20.0 + u_time * u_scrollSpeed));
+            float streaks = smoothstep(0.6, 0.9, n);
+            float alpha = streaks * u_streakIntensity;
+
+            gl_FragColor = vec4(vec3(0.8, 0.9, 1.0) * alpha, alpha);
+        }
+    `;
+
+  constructor() {
+    // Play ambient rain audio
+    let rainAudio = new Audio('rain_on_window.mp3'); 
+    rainAudio.loop = true;
+    rainAudio.play(); 
+
+    // Creating the window geometry
+    const windowGeometry = new THREE.PlaneGeometry(4,4);
+
+    const rainyWindowMaterial = new THREE.ShaderMaterial({
+        uniforms: {
+            u_time: { value: 0 },
+            u_noiseScale: { value: 20.0 },
+            u_streakIntensity: { value: 0.5 },
+            u_scrollSpeed: { value: 1.0},
+            u_seed: { value: Math.random()},
+        },
+        transparent: true,
+        vertexShader: RainyWindow.vertexShader,
+        fragmentShader: RainyWindow.fragmentShader,
+    });
+    super(windowGeometry, rainyWindowMaterial);
+    this.position.set(-4.1,4,4.6);
+    this.rotateY(Math.PI/2);
+    }
+}
